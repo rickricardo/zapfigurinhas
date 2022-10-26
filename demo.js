7ecb9df46af7118a126e56d52158f511b2987ede
rickricardo <69418631+rickricardo@users.noreply.github.com>
2022-10-26 12:02:13 +0200

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,50 @@
+/*jslint  browser: true, white: true, plusplus: true */
+/*global $, countries */
+
+$(function () {
+    'use strict';
+
+    var tagsArray = $.map(tags, function (value, key) { return { value: value, data: key }; });
+
+    // Setup jQuery ajax mock:
+    /*$.mockjax({
+        url: '*',
+        responseTime: 2000,
+        response: function (settings) {
+            var query = settings.data.query,
+                queryLowerCase = query.toLowerCase(),
+                re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi'),
+                suggestions = $.grep(tagsArray, function (country) {
+                     // return search.value.toLowerCase().indexOf(queryLowerCase) === 0;
+                    return re.test(country.value);
+                }),
+                response = {
+                    query: query,
+                    suggestions: suggestions
+                };
+
+            this.responseText = JSON.stringify(response);
+        }
+    });*/
+
+    // Initialize ajax autocomplete:
+    $('#autocomplete-ajax').autocomplete({
+        // serviceUrl: '/autosuggest/service/url',
+        lookup: tagsArray,
+        lookupFilter: function(suggestion, originalQuery, queryLowerCase) {
+            var re = new RegExp('\\b' + $.Autocomplete.utils.escapeRegExChars(queryLowerCase), 'gi');
+            return re.test(suggestion.value);
+        },
+        onSelect: function(suggestion) {
+            $('#selction-ajax').html('You selected: ' + suggestion.value + ', ' + suggestion.data);
+        },
+        onHint: function (hint) {
+            $('#autocomplete-ajax-x').val(hint);
+        },
+        onInvalidateSelection: function() {
+            $('#selction-ajax').html('You selected: none');
+        }
+    });
+
+   
+});
